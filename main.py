e145a9e0aa56eee4d84293c077502bca74b12fe8
papa <nekhunter35@gmail.com>
2026-08-05 13:58:45 +0000

    trier la liste de datasets ajoutés (partie 5.2)

--- main.py
+++ main.py
@@ -17,17 +17,23 @@ datasets = []
 
 # --- Partie 2 : Menu interactif (provisoire) ---
 while True:
+    print("\n")
     print("========================")
     print("1. Ajouter un dataset")
     print("2. Afficher les datasets")
-    print("3. Rechercher")
-    print("4. Quitter")
+    print("3. Trier les datasets (à venir)")
+    print("4. Rechercher un dataset (à venir)")
+    print("5. Modifier un dataset (à venir)")
+    print("6. Supprimer un dataset (à venir)")
+    print("7. Quitter")
     print("========================")
 
     choix = get_int("Votre choix : ")
 
+
+    # --- Partie 5.1 : Ajouter un dataset ---
     if choix == 1:
-        # --- Saisie des métadonnées du dataset ---
+        # --- Saisie des métadonnées du dataset + Ajouter un dataset ---
         nom = get_string("Nom du dataset : ")
 
         domaine = get_string(f"Domaine {DOMAINES_AUTORISES} : ")
@@ -52,7 +58,7 @@ while True:
             "public": public
         }
 
-        # --- Partie 5 : Ajout du dataset dans la liste ---
+        # --- Partie 5.1 : Ajout du dataset dans la liste ---
         datasets.append(dataset)
 
         # --- Affichage du résumé formaté du dataset ajouté ---
@@ -67,13 +73,53 @@ while True:
         print("==============================")
         print(f"Dataset '{dataset['nom']}' ajouté avec succès.\n")
 
-    elif choix == 2:
-        print("→ (à venir) Afficher les datasets\n")
 
+    # ---  Affichage de tous les datasets  ---
+    elif choix == 2:
+        if not datasets:
+            print("Aucun dataset disponible.\n")
+        else:
+            print("\n===== Liste des datasets =====")
+
+            for dataset in datasets:
+                print(f"Nom        : {dataset['nom']}")
+                print(f"Domaine    : {dataset['domaine']}")
+                print(f"Lignes     : {dataset['lignes']}")
+                print(f"Colonnes   : {dataset['colonnes']}")
+                print(f"Taille     : {dataset['taille']} Mo")
+                print(f"Format     : {dataset['format']}")
+                print(f"Public     : {'Oui' if dataset['public'] else 'Non'}")
+                print("------------------------------")
+
+
+    # --- Partie 5.2 : Trier les datasets  ---
     elif choix == 3:
-        print("→ (à venir) Rechercher un dataset\n")
+        if not datasets:
+            print("Aucun dataset disponible pour le tri.\n")
+        else:
+            datasets.sort(key=lambda dataset: dataset["nom"].lower())
+            print("Les datasets ont été triés par nom.\n")
+            print("\n===== Datasets triés par nom =====")
+
+        for dataset in datasets:
+            print(f"- {dataset['nom']}")
+
+        print("==================================\n")
+
 
     elif choix == 4:
+        # --- Partie 5.3 : Rechercher un dataset  ---
+        print("→ (à venir) Rechercher un dataset\n")
+
+    elif choix == 5:
+        # --- Partie 5.4 : Modifier un dataset  ---
+        print("→ (à venir) Modifier un dataset\n")
+
+    elif choix == 6:
+        # --- Partie 5.5 : Supprimer un dataset  ---
+        print("→ (à venir) Supprimer un dataset\n")
+
+    elif choix == 7:
         print("Au revoir !")
         break
 
