f40fa04ce18eb1b5a77ca48c82b5c85215af3add
papa <nekhunter35@gmail.com>
2026-08-06 13:38:04 +0000

    mise à jour

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ import csv
 
 from cs50 import get_string, get_int, get_float
 
-###############################################################################################################     # --- Partie 4 : Domaines autorisés | Tuples ---
+#*********************************************************************************************************************     # --- Partie 4 : Domaines autorisés | Tuples ---
 # --- Partie 4 : Domaines autorisés (immuables) ---
 DOMAINES_AUTORISES = ("Santé", "Finance", "Agriculture", "Transport", "Education")
 
@@ -91,8 +91,9 @@ def ajouter_dataset():
         == "true"
     )
 
-
-    # --- Partie 3 : Stockage dans un dictionnaire ---
+#*************************************************************************************************************    # --- Partie 3 : Stockage dans un dictionnaire ---
+# --- Partie 3 : Stockage dans un dictionnaire ---
+    
     dataset = {
         "nom": nom,
         "domaine": domaine,
@@ -103,7 +104,7 @@ def ajouter_dataset():
         "public": public
     }
 
-
+#*************************************************************************************************************    # --- Partie 5.1 : Ajout du dataset dans la liste ---
     # --- Partie 5.1 : Ajout du dataset dans la liste ---
     datasets.append(dataset)
 
@@ -717,66 +718,66 @@ def statistiques():
             "======================================\n"
         )
 
-###############################################################################################################     # --- Partie 1 : Menu interactif ---
+#*************************************************************************************************************    # --- Partie 2 : Menu interactif ---
 # --- Partie 2 : Menu interactif (provisoire) --- fonction 0
 while True:
     choix = afficher_menu()
 
-###############################################################################################################     # --- Partie 5.1 : Ajouter un dataset ---
+#*************************************************************************************************************     # --- Partie 5.1 : Ajouter un dataset ---
     # --- Partie 5.1 : Ajouter un dataset --- fonction 1
     if choix == 1:
         ajouter_dataset()
 
-###############################################################################################################     # --- Partie 5.1 : Ajouter un dataset ---
+#*************************************************************************************************************     # --- Partie 5.1 : Ajouter un dataset ---
     # ---  Affichage de tous les datasets  --- fonction 2
     elif choix == 2:
         afficher_datasets()
 
-###############################################################################################################     # --- Partie 5.2 : Trier les datasets  ---
+#*************************************************************************************************************     # --- Partie 5.2 : Trier les datasets  ---
 
     # --- Partie 5.2 : Trier les datasets  --- fonction 5
     elif choix == 3:
         trier_dataset()
 
-###############################################################################################################     # --- Partie 5.3 : Rechercher un dataset  ---
+#*************************************************************************************************************     # --- Partie 5.3 : Rechercher un dataset  ---
 
     # --- Partie 5.3 : Rechercher un dataset  --- fonction 4
     elif choix == 4:
         rechercher_dataset()
 
-###############################################################################################################    # --- Partie 5.4 : Modifier un dataset  ---
+#*************************************************************************************************************     # --- Partie 5.4 : Modifier un dataset  ---
 
     # --- Partie 5.4 : Modifier un dataset  --- fonction 6
     elif choix == 5:
         modifier_dataset()  
 
-###############################################################################################################     # --- Partie 5.5 : Supprimer un dataset  ---
+#*************************************************************************************************************    # --- Partie 5.5 : Supprimer un dataset  ---
 
     # --- Partie 5.5 : Supprimer un dataset  --- Fonction 3
     elif choix == 6:
         supprimer_dataset()
 
-###############################################################################################################     # --- Partie 6 : Statistiques ---
+#*************************************************************************************************************     # --- Partie 6 : Statistiques ---
 
     # --- Partie 6 : Statistiques --- fonction 9
     elif choix == 7:
         statistiques()
 
-###############################################################################################################     # --- Partie 7.1 : Sauvegarder les données dans un fichier CSV ---
+#*************************************************************************************************************       # --- Partie 7.1 : Sauvegarder les données dans un fichier CSV ---
 
         # --- Partie 7.1 : Sauvegarder les données dans un fichier CSV --- fonction 7
 
     elif choix == 8:
         sauvegarder()
 
-###############################################################################################################     # --- Partie 7.2 : Recharger + afficher les données depuis un fichier CSV ---
+#*************************************************************************************************************     # --- Partie 7.2 : Recharger + afficher les données depuis un fichier CSV ---
 
     # --- Partie 7.2 : Recharger + afficher les données depuis un fichier CSV --- fonction 8
 
     elif choix == 9:
         recharger()
 
-###############################################################################################################     # --- Quitter l'application ---
+#*************************************************************************************************************     # --- Quitter l'application ---
 
     elif choix == 10:
         print("Au revoir !")
